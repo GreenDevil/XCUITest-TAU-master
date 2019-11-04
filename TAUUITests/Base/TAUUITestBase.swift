8459ae7f3455cc9ac406e6372ecb28befbcd1537
PillowDust <e.dultzev@gmail.com>
2019-11-04 15:34:46 +0300

    add Base class UI Tests
    add TAUUITestBase.swift

---
+++ TAUUITests/Base/TAUUITestBase.swift
@@ -0,0 +1,37 @@
+import Foundation
+import XCTest
+import EyesXCUI
+
+class TAUUITestBase: XCTestCase {
+
+    var app = XCUIApplication()
+    
+    // Initialize the eyes SDK and set your private API key.
+    var eyes = Eyes()
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launch()
+        eyes.apiKey = "Nlovfgn8mV0kxX4sPaYaII9wpIUZawtNBmcFv596Vos110"
+        
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        app.terminate()
+        // End the test.
+        do {
+            try eyes.close()
+        } catch {
+            eyes.abortIfNotClosed()
+        }
+    }
+    
+    func XCTAsyncAssert(_ element: XCUIElement) {
+        let isElementExist = element.waitForExistence(timeout: 10)
+        if isElementExist {
+            XCTAssertTrue(element.exists)
+        }
+    }
+}
